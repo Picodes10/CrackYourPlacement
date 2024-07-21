710f00ad4c35474c62faed65776e180687910dd0
Sushree Manaswini Biswal <biswalsushree000@gmail.com>
2024-07-21 23:42:08 +0530

    Solve DAY-2  2 Problems

---
+++ DAY-2/Solution.java
@@ -0,0 +1,25 @@
+// Sort Color
+public class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        int curr = 0;
+        while (curr <= high) {
+            if (nums[curr] == 0) {
+                swap(nums, low, curr);
+                low++;
+                curr++;
+            } else if (nums[curr] == 2) {
+                swap(nums, high, curr);
+                high--;
+            } else {
+                curr++;
+            }
+        }
+    }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
---
+++ DAY-2/Solution1.java
@@ -0,0 +1,18 @@
+//  find the duplicate Number
+public class Solution1 {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        int ptr1 = nums[0];
+        int ptr2 = slow;
+        while (ptr1 != ptr2) {
+            ptr1 = nums[ptr1];
+            ptr2 = nums[ptr2];
+        }
+        return ptr1;
+    }
+}
